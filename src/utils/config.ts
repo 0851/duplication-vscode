c76ce8dd2a27ac544b438b4a7b2067983c499465
wangoo <importcode@gmail.com>
2020-09-21 21:10:16 +0800

    fix: 增加默认屏蔽路径

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -27,18 +27,21 @@ export class Config {
   }
   get ignore (): string[] {
     return this.data['duplication.ignore'] || [
-      'node_modules/**/*',
-      'bower_components',
-      'jspm_packages',
-      'web_modules',
-      '.cache',
-      '.history',
-      '.yarn/cache',
-      '.vscode-test',
-      'out',
-      'dist',
-      'build',
-      'logs'
+      "node_modules/**/*",
+      "bower_components",
+      "jspm_packages",
+      "web_modules",
+      ".cache",
+      ".history",
+      ".yarn/cache",
+      ".vscode-test",
+      ".idea",
+      ".vscode",
+      ".git",
+      "out",
+      "dist",
+      "build",
+      "logs"
     ];
   }
   get minTokens (): number {
